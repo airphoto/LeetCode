86a223b7689e8ff8d5a37d8a633b1ec0098afe18
lihuasong <lihuasong@ixianlai.com>
2021-01-20 23:15:28 +0800

    20210120

--- array_codes/src/Code766.java
+++ array_codes/src/Code766.java
@@ -38,37 +38,62 @@
  * matrix[i][j] 包含的整数在 [0, 99]范围内。
  *
  * 思考：
- *
+ *      解法一：将边界的数值保存到一个数组中，然后寻找两个数组的index之间的规律
+ *      解法二：
+ *      第i行    [0,n-1] 与
+ *      第i+1行的 [1,n]  相同
  * @Date 2021/1/19 21:43
  * @Version V1.0
  **/
 public class Code766 {
 
     public static void main(String[] args) {
-        int[][] matrix = new int[][] {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+//        int[][] matrix = new int[][] {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+        int[][] matrix = new int[][] {{1,2},{2,1},{1,2}};
         System.out.println(isToeplitzMatrix(matrix));
     }
 
     public static boolean isToeplitzMatrix(int[][] matrix) {
         int mlength = matrix[0].length;
-        int[] nums = new int[matrix.length+mlength-1];
+        int hlength = matrix.length;
+        int[] nums = new int[hlength+mlength-1];
         if(nums.length==1) {
             return true;
         }
 
         int index = 0;
-        int length = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int i1 = 0; i1 < matrix[i].length; i1++) {
                 if(i==0 || i1==0){
                     nums[index] = matrix[i][i1];
                     index ++;
-                } else if (nums[length - i*mlength -1] != matrix[i][i1]){
-                    return false;
                 }
-                length +=1;
             }
+        }
 
+        for (int i = 0; i < nums.length; i++) {
+            if(i<=mlength-1){
+                int h1 = 0;
+                int h2 = i;
+                while (h1<hlength && h2<mlength){
+                    if (nums[i] != matrix[h1][h2]){
+                        return false;
+                    }
+                    h1++;
+                    h2++;
+                }
+            }
+            if (i>mlength-1){
+                int h1 = i-mlength+1;
+                int h2 = 0;
+                while (h1<hlength && h2<mlength){
+                    if (nums[i] != matrix[h1][h2]){
+                        return false;
+                    }
+                    h1++;
+                    h2++;
+                }
+            }
         }
         return true;
     }
